f2a95f5098bcd892d8dfb08301e47795a06d2ab2
Mownika-Anandakumar <165825348+Mownika-Anandakumar@users.noreply.github.com>
2024-09-10 22:12:39 +0530

    Create evenNum.java

---
+++ EvenNum.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class EvenNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if(i % 2 == 0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
