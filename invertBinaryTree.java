cf0b6a1eb011fd6119685224394d6a1bec325799
Mownika Anandakumar <165825348+Mownika-Anandakumar@users.noreply.github.com>
2024-10-17 23:42:59 +0530

    Create invertBinaryTree.java

---
+++ invertBinaryTree.java
@@ -0,0 +1,90 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Main {
+    static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if (i >= ip.length)
+                break;
+            currVal = ip[i];
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void levelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Node tempNode = queue.poll();
+            System.out.print(tempNode.data + " ");
+            if (tempNode.left != null) {
+                queue.add(tempNode.left);
+            }
+            if (tempNode.right != null) {
+                queue.add(tempNode.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        Node root = buildTree(s);
+        Solution g = new Solution();
+        root = g.invert(root);
+        levelOrder(root);
+    }
+}
+
+class Solution {
+
+    public Node invert(Node root) {
+       //write your code
+       if(root==null){
+           return null;
+       }
+           Node left=invert(root.left);
+           Node right=invert(root.right);
+           root.left=right;
+           root.right=left;
+       return root;
+    }
+}
